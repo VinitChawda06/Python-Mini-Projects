fb51437d906ecb5a57ede8a216de1e0896352f56
VinitChawda06 <vinitchawda20@gmail.com>
2024-05-30 18:10:29 +0530

    Created a zigzagpattern animation file

---
+++ zigzagpattern.py
@@ -0,0 +1,23 @@
+import time, sys
+
+indent = 0
+indentIncreasing = True
+
+try:
+    while True:
+        print(' ' * indent , end='')
+        print('*******')
+        time.sleep(0.1)
+
+        if indentIncreasing:
+          indent += 1
+          if indent == 10:
+              indentIncreasing = False
+        else:
+            indent -= 1
+
+        if indent == 0:
+            indentIncreasing = True
+
+except KeyboardInterrupt:
+    sys.exit()
