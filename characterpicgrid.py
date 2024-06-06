81550d724a5299c693f54b7e4d9c8bfa6bdf46a6
VinitChawda06 <vinitchawda20@gmail.com>
2024-06-06 20:40:32 +0530

    Successfully completed the code and it prints the character pattern

---
+++ characterpicgrid.py
@@ -0,0 +1,19 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+['.', 'O', 'O', '.', '.', '.'],
+['O', 'O', 'O', 'O', '.', '.'],
+['O', 'O', 'O', 'O', 'O', '.'],
+['.', 'O', 'O', 'O', 'O', 'O'],
+['O', 'O', 'O', 'O', 'O', '.'],
+['O', 'O', 'O', 'O', '.', '.'],
+['.', 'O', 'O', '.', '.', '.'],
+['.', '.', '.', '.', '.', '.']]
+
+number_of_rows = len(grid) # 9
+number_of_columns = len(grid[0]) # 6
+
+for columns in range(number_of_columns):
+    for rows in range(number_of_rows):
+        print(grid[rows][columns], end="")
+    print()
+
+
