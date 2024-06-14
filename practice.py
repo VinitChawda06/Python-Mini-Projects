c13572f07ecaa8baf05fb9334913a154f2cfec1e
VinitChawda06 <vinitchawda20@gmail.com>
2024-06-14 21:15:39 +0530

    Created a chess dictionary validator file

--- practice.py
+++ practice.py
@@ -10,10 +10,21 @@
 # Test cases
 # spam = ['apples', 'bananas', 'tofu', 'cats']
 
-import pprint
-message = 'It was a bright cold day'
-count = {}
-for character in message:
-    count.setdefault(character, 0)
-    count[character] = count[character] + 1
-pprint.pprint(count)
+# import pprint
+# message = 'It was a bright cold day'
+# count = {}
+# for character in message:
+#     count.setdefault(character, 0)
+#     count[character] = count[character] + 1
+# pprint.pprint(count)
+
+allGuests = {'Alice': {'apples': 5, 'pretzels': 12},
+'Bob': {'ham sandwiches': 3, 'apples': 2},
+'Carol': {'cups': 3, 'apple pies': 1}}
+
+# def totalBrought(guests, item):
+numBrought = 0
+for k, v in allGuests.items():
+     print(k,v)
+    #  numBrought = numBrought + v.get(item, 0)
+# return numBrought
